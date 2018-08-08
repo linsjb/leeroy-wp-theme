5d56312738844a9ead41b63b3694ea2d9d057f4d
linsjb <linussjobro@gmail.com>
2018-08-09 00:34:37 +0200

    lang

--- dist/php/functions/language.php
+++ dist/php/functions/language.php
@@ -6,15 +6,15 @@
 * When the cookie is set the page will update. And the other language will be displayed.
 *
 */
+if(isset($_GET['lang'])) {
+  setcookie('language', $_GET['lang'], 0, COOKIEPATH, COOKIE_DOMAIN);
+  wp_redirect(home_url());
+  exit;
+}
+
 add_action('init', 'language');
 
 function language() {
-  if(isset($_GET['lang'])) {
-    setcookie('language', $_GET['lang'], 0, COOKIEPATH, COOKIE_DOMAIN);
-    
-    wp_redirect(home_url());
-    exit;
-  }
 
   if(!isset($_COOKIE['language'])) {
     setcookie('language', 'sv', 0, COOKIEPATH, COOKIE_DOMAIN);
