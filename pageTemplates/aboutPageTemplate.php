a730321d7dfcb3756f20a49a5e959010895b1c5f
linsjb <linussjobro@gmail.com>
2020-04-06 22:09:13 +0000

    Minor live changes

--- pageTemplates/aboutPageTemplate.php
+++ pageTemplates/aboutPageTemplate.php
@@ -166,18 +166,18 @@ if(have_posts()) {
           // Language control
           if(get_field('acfAboutSecLang')) {
             switch($_COOKIE['language']) {
-                case 'en':
-                  $formSectionTitle->setObject('acfAboutFormTitlePref', 'primLangTitle');
-                  break;
+              case 'en':
+                $formSectionTitle->setObject('acfAboutFormTitlePref', 'primLangTitle');
+                break;
 
-                case 'sv':
-                  $formSectionTitle->setObject('acfAboutFormTitlePref', 'secLangTitle');
-                  break;
+              case 'sv':
+                $formSectionTitle->setObject('acfAboutFormTitlePref', 'secLangTitle');
+                break;
 
-                default:
-                  $formSectionTitle->setObject('acfAboutFormTitlePref', 'primLangTitle');
-                  break;
-              }
+              default:
+                $formSectionTitle->setObject('acfAboutFormTitlePref', 'primLangTitle');
+                break;
+            }
           } else {
             $formSectionTitle->setObject('acfAboutFormTitlePref', 'primLangTitle');
           }
@@ -186,7 +186,16 @@ if(have_posts()) {
 
           $formSectionTitleAlignment = acfButtonGroup('textAlignment', 'acfAboutFormTitlePref', 'alignment', null, true);
           $formSectionTitleTextColor = acfButtonGroup('textColor', 'acfAboutFormTitlePref', 'color', null, true);
+          
           $formSectionTitle->setClasses('a-aboutPageSection__header ' . $formSectionTitleAlignment . ' ' . $formSectionTitleTextColor . ' ');
+          
+          $formSectionSubheader = new AcfText;
+          $formSectionSubheader->useSubfield();
+          $formSectionSubheader->setObject('acfAboutFormTitlePref', 'subheader');
+          $formSectionSubheader->setElementType('h4');
+
+          $formSectionSubheaderColor = acfButtonGroup('textColor', 'acfAboutFormTitlePref', 'subheaderColor', null, true);
+          $formSectionSubheader->setClasses('a-aboutPageSection__subheader ' . $formSectionTitleAlignment . ' ' . $formSectionSubheaderColor . ' ');
 
           $formSectionIcon = new AcfImage;
           $formSectionIcon->useSubfield();
@@ -197,11 +206,14 @@ if(have_posts()) {
           $formSectionShortcode = new AcfText;
           $formSectionShortcode->useSubfield();
           $formSectionShortcode->setObject('acfAboutFormShortcode');
+          
 
           $aboutSectionBackgroundColor = acfButtonGroup('backgroundColor', 'acfAboutFormSecPref', 'backgroundColor', null, true);
           echo '<div class="o-aboutPage col-xs-24 ' . $aboutSectionBackgroundColor . '" id="join-us">';
             echo '<div class="container o-aboutPageContent">';
               $formSectionTitle->init();
+              $formSectionSubheader->init();
+              
 
               echo '<div class="o-contactFormContent col-xs-24 col-sm-20">';
                 echo do_shortcode($formSectionShortcode->init());
